77d52c44a306901d5df0af8e9596565fcfd77034
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-06-29 09:49:22 -0300

    adicionado o styled component, e configurado o globalstyle

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+"use client"
+
+import { GlobalStyle } from "./assets/styles/global";
+
 export default function Home() {
   return (
-    <h1>Olar Mundo..</h1>
+    <>
+      <h1>Olar Mundo..</h1>
+      <GlobalStyle />
+    </>
   )
 }
