58a7409c4087470ed131dc0e76a0c6566e740373
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-06-29 11:02:05 -0300

    configurado um reset basico, e adicionado cores do projeto

--- app/assets/styles/global.ts
+++ app/assets/styles/global.ts
@@ -1,6 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  --primary: #1C3C50;
+  --blue-100: #F0F8FF;
+  --blue-500: #2C83FB;
+  --blue-800: #1F76F0;
+  --gray: #83979E;
+
+
   body {
     font-family: 'PlusJakartaSans', sans-serif;
     font-weight: 400;
