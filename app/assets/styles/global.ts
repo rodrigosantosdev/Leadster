43feb0706cb5a94b2dd916bf01cf02bee458f7da
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-06-29 13:37:20 -0300

    ajuste de variavel de cor

--- app/assets/styles/global.ts
+++ app/assets/styles/global.ts
@@ -7,15 +7,18 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
   }
 
-  --primary: #1C3C50;
-  --blue-100: #F0F8FF;
-  --blue-500: #2C83FB;
-  --blue-800: #1F76F0;
-  --gray: #83979E;
-
-
   body {
     font-family: 'PlusJakartaSans', sans-serif;
     font-weight: 400;
+    font-size: 1rem;
   }
 `
+
+export const colors = {
+  primary: "#506270",
+  blue100: "#F0F8FF",
+  blue500: "#2C83FB",
+  blue800: "#1F76F0",
+  gray: "#83979E",
+  hr: "#C8D4DD",
+}
