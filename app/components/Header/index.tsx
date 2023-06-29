d5434109c3c7ca65a0e8132da84b079c6029546a
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-06-29 11:20:58 -0300

    component header

---
+++ app/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { THeader } from "./style"
+import { Logo } from "../Logo"
+
+export const Header = () => {
+  return (
+    <THeader>
+      <Logo />
+    </THeader>
+  )
+}
---
+++ app/components/Header/style.ts
@@ -0,0 +1,8 @@
+import { styled } from 'styled-components';
+
+export const THeader = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 40px 0;
+`
