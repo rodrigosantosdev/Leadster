13bbf844884c71c454426bd37b2d26deff51a234
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-06-29 11:21:09 -0300

    component logo

---
+++ app/components/Logo/index.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image"
+import React from 'react'
+
+
+export const Logo = () => {
+  return (
+    <Image src="/images/logo.png" alt="logo" width={194} height={42} />
+  )
+}
