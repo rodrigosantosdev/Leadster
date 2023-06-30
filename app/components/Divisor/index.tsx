02cbeb3cd03af02b813f45ef681ab03b7ab28687
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-06-29 21:44:59 -0300

    component HR

---
+++ app/components/Divisor/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { MyHr } from "./style"
+
+export const Divisor = () => {
+  return (
+    <MyHr />
+  )
+}
---
+++ app/components/Divisor/style.ts
@@ -0,0 +1,10 @@
+import { colors } from "@/app/assets/styles/global";
+import { styled } from "styled-components";
+
+
+export const MyHr = styled.hr`
+  width: 100%;
+  border: 0;
+  height: 1px;
+  background: ${colors.hr};
+`
