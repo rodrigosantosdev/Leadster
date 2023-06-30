b14147989a69e163ea096bfe466583f011fdb2cb
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-06-30 13:10:42 -0300

    estruturei o page, com components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 "use client"
 
 import { GlobalStyle } from "./assets/styles/global";
+import { Footer } from "./components/Footer";
+import { GeracaoLeads } from "./components/GeracaoLeads";
+import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
+import { Videos } from "./components/Videos";
 
 export default function Home() {
   return (
     <>
-      <h1>Olar Mundo..</h1>
+      <Header />
+      <Hero />
+      <Videos />
+      <GeracaoLeads />
+      <Footer />
       <GlobalStyle />
     </>
   )
