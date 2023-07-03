220b5875d52113ced631b30fc2b0dddd1007bbdf
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-07-03 16:30:06 -0300

    ajuste de styles

--- app/components/Hero/style.ts
+++ app/components/Hero/style.ts
@@ -25,24 +25,32 @@ export const Tag = styled.span`
   border: 3px solid ${colors.blue500};
 
   font-weight: 800;
-  font-size: 14px;
+  font-size: 0.875rem;
   text-transform: uppercase;
   color: ${colors.blue500};
 `
 export const Subtitle = styled.h2`
-  font-size: 47px;
+  font-size: 2.9375rem;
   font-weight: 600;
   color: ${colors.primary};
   margin-top: 24px;
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
 `
 
 export const Title = styled.h1`
-  font-size: 87px;
+  font-size: 5.4375rem;
   background: -webkit-linear-gradient(-45deg, #2C83FB, #1F76F0);
   background-clip: text;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   position: relative;
+
+  @media (max-width: 768px) {
+    font-size: 3rem;
+  }
 
   &::after {
     content: '-';
@@ -59,8 +67,14 @@ export const Title = styled.h1`
 export const Description = styled.p`
   color: ${colors.primary};
   font-weight: 600;
-  font-size: 17.7px;
+  font-size: 1.1062rem;
   margin-top: 20px;
+
+  @media (max-width: 768px) {
+    max-width: 360px;
+    text-align: center;
+    font-size: 0.875rem;
+  }
 `
 
 
